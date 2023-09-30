74b55f88bf3ed0c509314c49a0d3f47451020ea3
Ravula Mohanish <ravula.mohanish@gnapitech.com>
2023-09-30 21:16:14 +0530

    Revert "third java commit in helloworld"
    This reverts commit 071fbe81dfb1380f5d6d0cf85aa6d6db3502492d.

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,7 @@ public class helloworld {
 
             // Press Alt+Shift+D to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+Shift+B.
-            System.out.println("i = " + 2);
+            System.out.println("i = " + i);
         }
     }
 }
-
-
-for(condition){
-    //statement
-        }
